1b47a68cce22eff9f3227f91f5b208aa21fa0172
VOID-Dagger <86911038+VOID-Dagger@users.noreply.github.com>
2021-08-21 16:34:15 +0000

    Update CPU.sh

--- CPU.sh
+++ CPU.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo apt update
-sudo apt install screen -y
+apt update
+apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-
 x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
